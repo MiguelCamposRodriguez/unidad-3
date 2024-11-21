2b425d7afd838892065f5db25c5582e1d929ce6d
MiguelCamposRodriguez <miguelcrgamin123@gmail.com>
2024-11-21 12:57:08 -0600

    Add files via upload

---
+++ E.R.6/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace E.R._6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(110, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(297, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Verifica si un numero es par o impar";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(68, 157);
+            label2.Name = "label2";
+            label2.Size = new Size(135, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Escribir numero";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(110, 322);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Verifica";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(473, 134);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(180, 279);
+            listBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(209, 154);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private ListBox listBox1;
+        private TextBox textBox1;
+    }
+}
